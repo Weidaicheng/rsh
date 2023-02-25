5c9582ff0372572a6b42aa6abbcdee37afac5615
Weidaicheng <wdcdavyc@outlook.com>
2023-02-25 21:14:42 +0800

    multi commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-use std::{io::stdin, process::Command};
+use std::{io::{stdin, stdout, Write, Error}, process::Command};
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input)
-        .unwrap();
+    loop {
+        print!("> ");
+        if let Err(e) = stdout().flush() {
+            handle_error(e);
+        }
 
-    let command = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input)
+            .unwrap();
 
-    Command::new(command)
-        .spawn()
-        .unwrap();
+        let command = input.trim();
+
+        let child = Command::new(command)
+            .spawn();
+        
+        match child {
+            Ok(mut child) => {
+                if let Err(e) = child.wait() {
+                    handle_error(e);
+                }
+            },
+            Err(e) => handle_error(e)
+        };
+    }
+}
+
+fn handle_error(e: Error) {
+    eprintln!("{}", e);
 }
